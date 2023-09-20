0f73905ed050cce7c951bb32ad6afaadb503a0d6
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-09-20 13:15:41 -0500

    feat: :sparkles: navigation routes
    with private and protected routes

---
+++ src/routes/App.jsx
@@ -0,0 +1,42 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from '../modules/auth/components/Login';
+import NotFound from '@/modules/auth/components/NotFound';
+import { AuthProvider, AuthRoute } from '@/modules/auth/context/auth'
+import { Logout } from '@/modules/auth/components/Logout';
+import Home from '../modules/home/Home';
+import SuperAdmin from '../modules/pages/superAdmin/components/SuperAdmin';
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <AuthProvider>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/Login" element={<Login />} />
+              <Route
+              path="/SuperAdmin"
+              element={
+                <AuthRoute>
+                  <SuperAdmin />
+                </AuthRoute>
+              }
+            />
+            <Route
+              path="/logout"
+              element={
+                <AuthRoute>
+                  <Logout />
+                </AuthRoute>
+              }
+            />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </AuthProvider>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
