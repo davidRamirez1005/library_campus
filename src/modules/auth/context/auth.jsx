7aa47174946a9788c0532894611d8dc026310974
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-09-20 13:14:38 -0500

    feat: :safety_vest: auth context
    to share login status information between auth components

---
+++ src/modules/auth/context/auth.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+
+
+const AuthContext = React.createContext();
+
+function AuthProvider({ children }) {
+    const navigate = useNavigate();
+    const [user, setUser] = React.useState(null);
+
+    const logins = ({ username, name }) => {
+        setUser({ username, name });
+        navigate("/SuperAdmin");
+};
+const logout = () => {
+    setUser(null);
+    navigate("/");
+};
+
+const auth = { user, logins, logout };
+
+return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
+}
+
+function useAuth() {
+    const auth = React.useContext(AuthContext);
+    return auth;
+}
+
+function AuthRoute(props) {
+    const auth = useAuth();
+
+if (!auth.user) {
+    return <Navigate to="/login" />;
+}
+
+return props.children;
+}
+
+export { AuthProvider, AuthRoute, useAuth };
