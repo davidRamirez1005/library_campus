1b34d99a04b906ca1335cba02e7ce3c7aca01b2c
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-09-22 11:42:17 -0500

    build: :construction_worker: environment variables are set for vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+const env = loadEnv("development", process.cwd(), 'VITE')
+
+console.log(env);
 export default defineConfig({
   plugins: [react()],
+  'react-bootstrap': 'react-bootstrap/esm',
   resolve: {
-    alias: {
-      '@': '/src',
-      'react-bootstrap': 'react-bootstrap/esm'
-    }
+        alias: {
+          '@': '/src',
+        }
+  },
+  server:{
+    port: env.VITE_PORT_FRONTEND,
+    host: env.VITE_HOSTNAME,
   }
-});
+})
