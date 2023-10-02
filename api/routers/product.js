5ccacae2877357c6ecdf8c5ce13ddfd3f899fb13
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-10-02 01:13:28 -0500

    feat: :sparkles: enpoint with version

--- api/routers/product.js
+++ api/routers/product.js
@@ -1,8 +1,8 @@
 import {limitget} from '../config/configLimit.js'
 import Routes from 'express';
 import routesVersioning  from 'express-routes-versioning';
-import {getProductAvailable, getAvaliableBooks, getBorrowed, getHistoryUser, getProductCloseDate, getBooksDelivered, getBooksReserved} from '../controllers/v1/productV1.js'
-import {aggregateNewProduct, updateStatusDelivery, updateStatusSpend, updateStatusAvailable, updateStatusBorrowed, deleteProduct, updateBookUser} from '../controllers/v2/productV2.js'
+import {getProductAvailable, getAvaliableBooks, getBorrowed, getHistoryUser, getProductCloseDate, getBooksDelivered, getBooksReserved, historyUser, favoriteProduct} from '../controllers/v1/productV1.js'
+import {aggregateNewProduct, updateStatusDelivery, updateStatusSpend, updateStatusAvailable, updateStatusBorrowed, deleteProduct, updateBookUser, updateBookUserReserved, aggregateProductHistory} from '../controllers/v2/productV2.js'
 // import {aggregateNewAdmin, deleteAdmin} from '../controllers/v2/adminV2.js'
 
 const appProduct = Routes();
@@ -25,21 +25,29 @@ appProduct.get('/listar/entregados',  version({
 appProduct.get('/listar/reservados',  version({
     "~1.0.0": getBooksReserved,
 }))
+/**
+ * !history
+ */
+appProduct.get('/history/:id_user?',  version({
+    "~1.0.0": historyUser,
+    "~1.1.0": favoriteProduct,
+}))
 
 appProduct.post('/agregar/producto', version({
     "~1.0.0": aggregateNewProduct,
-    // "~1.1.0": aggregaProductBorrowed 
+    "~1.1.0": aggregateProductHistory 
 }))
 // appProduct.post('/agregar/admin', version({
 //     "~1.2.0": aggregateNewAdmin
 // }))
 
-appProduct.put('/actualizar/producto/:product_id?', version({
+appProduct.put('/actualizar/producto/:product_id?/:identification?', version({
     "~1.0.0": updateStatusDelivery,
     "~1.1.0": updateStatusSpend,
     "~1.2.0": updateStatusAvailable,
     "~1.3.0": updateStatusBorrowed,
-    "~1.4.0": updateBookUser
+    "~1.4.0": updateBookUser,
+    "~1.5.0" : updateBookUserReserved
 }))
 
 appProduct.delete('/eliminar/producto/:product_id?', version({
